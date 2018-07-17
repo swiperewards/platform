20b9bb8f5d84059f19ce8f66d8ca6138289419a7
Proshonjit Das <ptxdas@gmail.com>
2018-07-16 22:18:15 -0700

    material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import './scss/styles.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          Start of Swipe Rewards
+            <Button variant="contained" color="primary">
+              Hello World
+            </Button>
       </div>
     );
   }
