a10d5a90237eae774112e07d8caba143e5ff13f4
Vinay <vinay@vinays-mbp-2.winjittechnologiespvtltd.local>
2018-07-19 19:02:45 +0530

    Added routes for forgot password as well as register containers

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, Switch, HashRouter } from 'react-router-dom'
 // CONTAINERS
 import Login from './containers/login'
 import ForgetPassword from './containers/forgotPassword'
+import Register from './containers/register'
 
 import App from './App'
 
@@ -18,6 +19,7 @@ class Routes extends Component {
                     <Switch>
                         <Route exact path="/resetPassword" component={ForgetPassword} />
                         <Route exact path="/resetPassword/:token" component={ForgetPassword} />
+                        <Route exact path="/register" component={Register} />
                         <Route exact path="/" component={Login} />
                         <Route exact path="/login" component={Login} />
 
