45b9ad6d119508106c1ce5cf987f6ea8d5394658
Vinay <vinay@vinays-mbp-2.winjittechnologiespvtltd.local>
2018-07-19 18:59:25 +0530

    Completed design for forgot password container

--- src/containers/forgotPassword.js
+++ src/containers/forgotPassword.js
@@ -18,34 +18,6 @@ const required = value => value && value.trim() !== "" ? undefined : `Required`
 
 
 const styles = {
-    loginContainer: {
-        minWidth: 320,
-        maxWidth: 400,
-        height: 'auto',
-        position: 'absolute',
-        top: '10%',
-        left: 0,
-        right: 0,
-        margin: 'auto'
-    },
-    paper: {
-        background:'#fff',
-        padding: '20px 25px',
-        margin: '5px',
-        overflow: 'auto',
-        borderRadius:'1px',
-        boxShadow:'0 0 6px #ccc'
-    },
-    logo:{
-        float:'left', 
-        textAlign: 'center', 
-        width: '100%'
-    },
-    label:{
-        color:'#231f20',
-        fontWeight:'normal',
-        fontSize:'14px'
-    },
     formContent:{
         float:'left',
         paddingTop:'0px', 
@@ -53,22 +25,6 @@ const styles = {
     },
     formGroup:{
         marginBottom:'10px'
-    },
-    loginTxt:{
-        fontFamily: 'open_sansbold',
-        paddingBottom:'10px',
-        fontSize: 17,
-        marginTop:'0px',
-        textAlign: 'left',
-        textTransform:'uppercase'
-    },
-    forgotTxt :{
-        color:'#424242',
-        float:'right',
-        fontStyle:'italic',
-        textDecorator:'underline',
-        fontSize:'14px',
-        fontWeight:'normal'
     },
     signUpTxt :{
         color:'#424242',
@@ -92,24 +48,24 @@ class ForgotPassword extends Component {
 
         return (
             <div>
-                <div style={styles.loginContainer}>
-                    <Paper style={styles.paper}>
-                        <div style={styles.logo}>
+                <div className="pageContainer">
+                    <Paper className="pagePaper">
+                        <div className="logo">
                             <img src="../images/logo1.png" alt="Logo" />
                         </div>
                         <div style={styles.formContent}>
                             <form size='large' className="form-horizontal" onSubmit={this.props.handleSubmit((event) => this.onSubmit(event))}>
-                                <div className="gradient-text" style={styles.loginTxt}>Forgot Password
+                                <div className="titleLabel">Forgot Password
                                 </div>
                                 <div style={styles.formGroup}>
-                                    <label style={styles.label}>Enter Your Register Email</label>
+                                    <label className="controlLabel">Enter Your Register Email</label>
                                     <InputField name="username" myLabel="Email" myPlaceHolder="Email" component={InputField} validate={required} />
                                 </div>
                                 <div style={{paddingTop:'10px', textAlign:'center'}}> 
                                     <RaiseButton style={{display:'inline-block' , float:'none'}} variant="contained" color="primary" label="SENT MAIL"/>
                                 </div>        
                                 <div style={styles.accountTxt}>
-                                        <span style={styles.label}>Don’t have an account?</span><span style={styles.signUpTxt}><Link to='/signup' style={styles.signupLink}> Register Now</Link>
+                                        <span style={styles.label}>Don’t have an account?</span><span style={styles.signUpTxt}><Link to='/register' style={styles.signupLink}> Register Now</Link>
                                         </span>
                                 </div>
                             </form>
