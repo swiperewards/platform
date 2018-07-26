3dffb94922d76158cbce633d28308c85d60b1304
Vinay <vinay@Vinays-MacBook-Pro-2.local>
2018-07-27 03:30:56 +0530

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import './scss/styles.css';
 import LeftBar from './components/leftBar'
 import Login from './containers/login';
 
+import {LARGE} from '@material-ui/core/utils/ownerWindow'
+
 class App extends Component {
 
   constructor(props) {
 
     super(props);
     this.state = {
+      navDrawerOpen: window.innerWidth >= 1025 ? true : false,
       loadStatus: true
     };
   }
@@ -20,26 +23,60 @@ class App extends Component {
     this.setState({ loadStatus: true })
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({ loadStatus: false })
+
+    if (this.props.width !== nextProps.width) {
+      this.setState({ navDrawerOpen: nextProps.width === LARGE });
+    }
+  }
+
+  handleChangeRequestNavDrawer() {
+
+    this.setState({
+      navDrawerOpen: !this.state.navDrawerOpen
+    });
+  }
+
+  updateDimensions() {
+    if (window.innerWidth <= 1025) {
+      this.setState({
+        navDrawerOpen: false
+      });
+    } else {
+      this.setState({
+        navDrawerOpen: true
+      });
+    }
+  }
+
+  handleBodyClick() {
+    if (window.innerWidth <= 1025) {
+      this.setState({
+        navDrawerOpen: false
+      });
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions.bind(this));
+  }
+
   render() {
     
-    const styles = {
-      container: {
-        margin: '80px 20px 20px 15px',
-      },
-    };
-
-
-    //#region Check whether logged in user is admin or other according that load side bar
-    if (this.props.userData) {
-      //const admin = this.props.userData.role === "superadmin" ? true : false;    
-      
+    let { navDrawerOpen } = this.state;
+    
+    if (this.props.userData) {      
       return(
             <div>
-              <LeftBar />
-
-              <div style={styles.container}>
-                  {this.props.children}
-              </div>
+              <LeftBar navDrawerOpen={window.innerWidth > 1025 ? true : navDrawerOpen}
+              handleChangeRequestNavDrawer={this.handleChangeRequestNavDrawer.bind(this)}
+              children={this.props.children}
+              />
             </div>
       );
     }
