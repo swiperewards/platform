f40d770dd983e3b649515ee38c561c90b2a86ff9
Vinay <vinay@Vinays-MacBook-Pro-2.local>
2018-07-26 21:47:31 +0530

    Added more routes to class

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import { Route, Switch, HashRouter } from 'react-router-dom'
 
+// authorization wrapper
+import { userIsAuthenticated } from './components/authorization'
+
 // CONTAINERS
 import Login from './containers/login'
 import ForgetPassword from './containers/forgotPassword'
 import Register from './containers/register'
+import Logout from './containers/logout'
+import ManageMerchants from './containers/merchant/manageMerchants'
+import ManageDeals from './containers/admin/manageDeals'
+
+
+import SuperAdminDashboard from './containers/superAdmin/superAdminDashboard'
+import AdminDashboard from './containers/admin/adminDashboard'
 
 import App from './App'
 
@@ -22,9 +32,13 @@ class Routes extends Component {
                         <Route exact path="/register" component={Register} />
                         <Route exact path="/" component={Login} />
                         <Route exact path="/login" component={Login} />
+                        <Route exact path="/logout" component={Logout} />
 
                         <App>
-                            
+                            <Route exact path="/admindashboard" component={userIsAuthenticated(AdminDashboard)} />
+                            <Route exact path="/superAdminDashboard" component={SuperAdminDashboard} />
+                            <Route exact path="/managemerchants" component={ManageMerchants} />
+                            <Route exact path="/managedeals" component={ManageDeals} />
                         </App>
                     </Switch>
                 </div>
