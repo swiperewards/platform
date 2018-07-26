e2ea110f15b70f55ff22fcea37aed3b4905cd663
Vinay <vinay@Vinays-MacBook-Pro-2.local>
2018-07-26 21:51:02 +0530

    Updated store instance in class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import configureStore from './store/store';
+import Store from './store/store';
 import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import defaultTheme from './scss/theme-default'
 import '../src/scss/styles.css'
 
-const store = configureStore();
-
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={Store}>
         <MuiThemeProvider theme={defaultTheme}>    
             <Routes />
         </MuiThemeProvider>
