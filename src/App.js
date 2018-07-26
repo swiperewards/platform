5341cb098ba5659d0c916765d07b6be36f031ccc
Vinay <vinay@Vinays-MacBook-Pro-2.local>
2018-07-26 21:45:29 +0530

    Added user logged in state check to navigate to appropriate screen.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import './scss/styles.css';
 
-import Login from './containers/login'
+import LeftBar from './components/leftBar'
+import Login from './containers/login';
 
 class App extends Component {
+
+  constructor(props) {
+
+    super(props);
+    this.state = {
+      loadStatus: true
+    };
+  }
+
+  componentWillMount() {
+    this.setState({ loadStatus: true })
+  }
+
   render() {
-    return (
+    
+    const styles = {
+      container: {
+        margin: '80px 20px 20px 15px',
+      },
+    };
+
+
+    //#region Check whether logged in user is admin or other according that load side bar
+    if (this.props.userData) {
+      //const admin = this.props.userData.role === "superadmin" ? true : false;    
+      
+      return(
+            <div>
+              <LeftBar />
+
+              <div style={styles.container}>
+                  {this.props.children}
+              </div>
+            </div>
+      );
+    }
+    else{
+      return (        
             <Login/>
-    );
+      );
+    }
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    userData: state.account === undefined ? undefined : state.account
+  }
+}
+
+export default connect(mapStateToProps, null)(App);
