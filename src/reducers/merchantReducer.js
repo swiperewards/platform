9d761c22ae22945c32d68eaf4084e3fdf37eee5f
Vinay <vinay@vinays-mbp-2.winjittechnologiespvtltd.local>
2018-08-22 10:59:05 +0530

    Added more cases for reducer to return payload

--- src/reducers/merchantReducer.js
+++ src/reducers/merchantReducer.js
@@ -2,8 +2,16 @@ export default function (state = {}, action) {
     switch (action.type) {
         case 'ONBOARD_MERCHANT':
             return {
-                ...state, data: action.payload
+                data: action.payload
             }
+        case 'GET_MERCHANTS':
+            return{
+                merchantList: action.payload
+            }  
+        case 'DELETE_MERCHANT':
+            return{
+                deleteMerchant: action.payload
+            }      
         default:
             return state;
     }
